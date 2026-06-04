8417efe44c0623fd11639711cdcda5cfde0f9937
DjoCoding <djocoding@gmail.com>
2026-06-04 08:03:15 +0100

    feat: add vercel.json

--- src/routes/_protected/d/_rfid/patients/$patientId/drafts.tsx
+++ src/routes/_protected/d/_rfid/patients/$patientId/drafts.tsx
@@ -1,9 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_protected/d/_rfid/patients/$patientId/drafts')({
+export const Route = createFileRoute(
+  "/_protected/d/_rfid/patients/$patientId/drafts",
+)({
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  return <div>Hello "/_protected/patients/$patientId/drafts"!</div>
+  return <div></div>;
 }
